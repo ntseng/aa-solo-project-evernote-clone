c40e4fb3c81ea1906ded96886b101a28a23123f4
nathaniel <nathanieltseng@gmail.com>
2022-01-06 09:58:29 -0800

    request body validation and login validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { check } = require("express-validator");
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require("../../utils/validation.js");
 
 const router = express.Router();
 
-router.post('/', asyncHandler(async (req, res, next) => {
+const validateLogin = [
+	check("credential")
+		.exists({ checkFalsy: true })
+		.notEmpty()
+		.withMessage("Please provide a valid email or username"),
+	check("password")
+		.exists({ checkFalsy: true })
+		.withMessage("Pleaes provide a password"),
+	handleValidationErrors
+];
+
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
 	const { credential, password } = req.body;
 
 	const user = await User.login({ credential, password });
