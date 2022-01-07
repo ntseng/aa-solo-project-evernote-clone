81cb85b312a445ca4025fc51304b30995044da33
nathaniel <nathanieltseng@gmail.com>
2022-01-07 10:44:14 -0800

    heroku config (#9)

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,28 +10,4 @@ router.post("/test", function (req, res) {
 	res.json({ requestBody: req.body });
 })
 
-if (process.env.NODE_ENV === "production") {
-	const path = require("path");
-	router.get("/", (req, res) => {
-		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.sendFile(
-			path.resolve(__dirname, "../../frontend", "build", "index.html")
-		)
-	})
-
-	router.use(express.static(path.res("../frontend/build")));
-
-	router.get(/^(?!\/?api).*/, (req, res) => {
-		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.sendFile(
-			path.resolve(__dirname, "../../frontend", "build", "index.html")
-		)
-	})
-} else {
-	router.get("/csrf/restore", (req, res) => {
-		res.cookie("XSRF-TOKEN", req.csrfToken());
-		return res.json({});
-	})
-}
-
 module.exports = router;
