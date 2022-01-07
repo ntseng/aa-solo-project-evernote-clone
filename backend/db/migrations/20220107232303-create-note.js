b906090fb040758b8d88b00769e40034316be862
nathaniel <nathanieltseng@gmail.com>
2022-01-07 15:31:20 -0800

    Create Notes and Notebooks models (#32)

---
+++ backend/db/migrations/20220107232303-create-note.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Notes', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: "Users" }
+			},
+			notebookId: {
+				type: Sequelize.INTEGER
+			},
+			title: {
+				type: Sequelize.STRING
+			},
+			content: {
+				type: Sequelize.TEXT
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Notes');
+	}
+};
