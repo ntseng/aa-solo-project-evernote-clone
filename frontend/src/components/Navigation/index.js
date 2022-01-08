12e2be6c578b086440aa7a96c8203c4e09926d84
nathaniel <nathanieltseng@gmail.com>
2022-01-07 17:05:00 -0800

    Landing Page: progress on top navbar  (#21)

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import { demoLogin } from "../../store/session";
 import ProfileButton from "./ProfileButton";
 
+import "./NavBar.css";
+
 export default function Navigation({ isLoaded }) {
 	const dispatch = useDispatch();
 	const sessionUser = useSelector(state => state.session.user);
@@ -14,19 +16,30 @@ export default function Navigation({ isLoaded }) {
 		)
 	} else {
 		sessionLinks = (
-			<>
-				<button onClick={e => dispatch(demoLogin())}>Demo Login</button>
-				<NavLink to="/login">Login</NavLink>
-				<NavLink to="/signup">Sign Up</NavLink>
-			</>
+			<nav id="utility-nav">
+				<span className="nav-element" onClick={e => dispatch(demoLogin())}>Demo Login</span>
+				<NavLink className="nav-element" id="login" to="/login">Log In</NavLink>
+				<button className="nav-element" id="github-button">GitHub</button>
+			</nav>
 		)
 	}
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-				{isLoaded && sessionLinks}
-			</li>
-		</ul>
+		<>
+			<header id="navbar">
+				<div className="centered nav-element-container">
+					<li>
+						<NavLink exact to="/"><img id="logo" src={"/images/logo.svg"} alt="Evernote Clone logo" /></NavLink>
+					</li>
+					<li className="nav-element">
+						<span>Developer</span>
+					</li>
+					<li className="nav-element">
+						<span>Tech Stack</span>
+					</li>
+					{isLoaded && sessionLinks}
+				</div>
+			</header>
+			<div id="calendar-banner"></div>
+		</>
 	)
 }
