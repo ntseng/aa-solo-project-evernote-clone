e4eb46421af4e790c8ed52be5c98cf55c3294e81
nathaniel <nathanieltseng@gmail.com>
2022-01-10 13:46:07 -0800

    Backend - Notes: Create (#11)

--- frontend/src/store/notes.js
+++ frontend/src/store/notes.js
@@ -2,6 +2,7 @@ import { csrfFetch } from "./csrf";
 
 const INITIAL_STATE = { notes: {} };
 const GET_NOTES = "notes/get";
+const ADD_NOTE = "notes/add";
 
 const setNotes = notes => ({
 	type: GET_NOTES,
@@ -19,6 +20,29 @@ export function getNotes({ id }) {
 	}
 }
 
+const addNote = note => ({
+	type: ADD_NOTE,
+	note
+})
+
+export function createNote({ userId, notebookId }) {
+	return async dispatch => {
+		const response = await csrfFetch("/api/notes", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				userId,
+				notebookId
+			})
+		})
+
+		if (response.ok) {
+			const { note } = await response.json();
+			dispatch(addNote(note));
+		}
+	}
+}
+
 export default function notesReducer(state = INITIAL_STATE, action) {
 	let updatedState = { ...state };
 	switch (action.type) {
@@ -26,6 +50,9 @@ export default function notesReducer(state = INITIAL_STATE, action) {
 			action.notes.forEach(note => {
 				updatedState.notes[note.id] = note;
 			})
+			return updatedState;
+		case ADD_NOTE:
+			updatedState.notes[action.note.id] = action.note;
 			return updatedState;
 		default:
 			return state;
