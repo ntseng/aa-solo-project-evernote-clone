a3f1289aba50ea92708b3a5bde2a07a26ee23ea6
nathaniel <nathanieltseng@gmail.com>
2022-01-10 09:31:02 -0800

    Login Page Elements (#23)

---
+++ frontend/src/components/AuthContainer/index.js
@@ -0,0 +1,21 @@
+import LoginFormPage from "../LoginFormPage";
+import SignUpFormPage from "../SignUpFormPage";
+
+import "./AuthContainer.css";
+
+export default function AuthContainer({ newAccount }) {
+	return (
+		<div id="contianer-boundingbox">
+			<div id="container">
+				<div className="main">
+					{newAccount ? <SignUpFormPage /> : <LoginFormPage />}
+				</div>
+				<div className="footer wrapper">
+					<span id="tos-link">Terms of Service</span>
+					<span id="privacy-link">Privacy Policy</span>
+					<span class="footer-entry">Student Project by Nathaniel Tseng. No rights claimed.</span>
+				</div>
+			</div>
+		</div>
+	)
+}
