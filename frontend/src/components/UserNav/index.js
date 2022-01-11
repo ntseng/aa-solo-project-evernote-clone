43642983fff74136db0641149e534a4a6ce256f8
nathaniel <nathanieltseng@gmail.com>
2022-01-11 14:13:40 -0800

    usernav styling (#26)

--- frontend/src/components/UserNav/index.js
+++ frontend/src/components/UserNav/index.js
@@ -1,19 +1,20 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { createNote } from "../../store/notes";
 import "./UserNav.css";
 
 export default function UserNav({ userId }) {
 	const dispatch = useDispatch();
+	const user = useSelector(state => state.session.user);
 
 	return (
 		<div id="user-nav">
 			<li id="settings">
-				<img alt="profile" />
-				<div>Placeholder</div>
+				<img id="profile-img" src="https://www.evernote.com/shard/s647/user/231928776/photo?size=56" alt="profile" />
+				<span id="username-span">{user.username}</span>
 			</li>
 			{/* <li>Search</li> */}
-			<li className="user-nav-li no-bullet"><button onClick={e => dispatch(createNote({ userId, notebookId: null }))}>New Note</button></li>
+			<li className="user-nav-li no-bullet"><button id="new-note-button" onClick={e => dispatch(createNote({ userId, notebookId: null }))}>+ New Note</button></li>
 			<li className="user-nav-li no-bullet">
 				<NavLink to={`/user`} className="user-nav-link">Home</NavLink>
 			</li>
@@ -25,8 +26,8 @@ export default function UserNav({ userId }) {
 					<NavLink to={`/notebooks`} className="user-nav-link">Notebooks</NavLink>
 				</li>
 			</ul>
-			<button>Upgrade</button>
-			<li>Get Started</li>
+			<a id="money-button" href="https://github.com/sponsors/ntseng">Monetization Button</a>
+			<a id="get-started" href="https://github.com/ntseng/aa-solo-react-project/blob/main/README.md">Get Started</a>
 		</div>
 	)
 }
