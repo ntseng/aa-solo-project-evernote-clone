112c8a3662540372f8f525aee57925dbc5ea6f72
nathaniel <nathanieltseng@gmail.com>
2022-01-11 15:58:56 -0800

    finished stying on value prop and hero row (#22)

--- frontend/src/components/Landing/valueProp.js
+++ frontend/src/components/Landing/valueProp.js
@@ -5,7 +5,7 @@ export default function ValueProp() {
 		<div id="value-prop-div">
 			<h1 id="title-h1">Tame your work, organize your life</h1>
 			<h5 id="description-h5">Remember everything and tackle any project with your notes, tasks, and schedule all in one place.</h5>
-			<p>
+			<p id="cta-p">
 				<Link className="cta" to="/signup">Sign up for free</Link>
 			</p>
 			<p className="login-cta">
