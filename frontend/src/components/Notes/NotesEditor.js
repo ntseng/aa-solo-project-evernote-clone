1129bee96b5fe58223b2a39ead6f133d021cd46b
nathaniel <nathanieltseng@gmail.com>
2022-01-11 10:36:41 -0800

    Notes Editor: Update note on change focus from title input or content input (#78)

--- frontend/src/components/Notes/NotesEditor.js
+++ frontend/src/components/Notes/NotesEditor.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { editNote } from "../../store/notes";
 
 export default function NotesEditor() {
+	const dispatch = useDispatch();
 	let note = useSelector(state => state.notes.currentNote);
 	const [title, setTitle] = useState("");
 	const [content, setContent] = useState("");
@@ -13,8 +15,16 @@ export default function NotesEditor() {
 
 	return (
 		<div>
-			<input placeholder="Title" value={title} onChange={e => setTitle(e.target.value)}></input>
-			<textarea placeholder="Start writing" value={content} onChange={e => setContent(e.target.value)}></textarea>
+			<input placeholder="Title"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+				onBlur={e => dispatch(editNote({ noteId: note.id, notebookId: null, title, content }))}
+			/>
+			<textarea placeholder="Start writing"
+				value={content}
+				onChange={e => setContent(e.target.value)}
+				onBlur={e => dispatch(editNote({ noteId: note.id, notebookId: null, title, content }))}
+			/>
 		</div>
 	)
 }
