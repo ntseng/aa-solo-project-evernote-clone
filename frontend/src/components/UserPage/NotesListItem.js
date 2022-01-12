c668de44a88cddb9deb0ebbfe230c821a45fdc34
nathaniel <nathanieltseng@gmail.com>
2022-01-12 14:33:55 -0800

    Frontend - Note View: Styling Fixes #28

--- frontend/src/components/UserPage/NotesListItem.js
+++ frontend/src/components/UserPage/NotesListItem.js
@@ -6,15 +6,19 @@ export default function NotesListItem({ note }) {
 	const history = useHistory();
 	const dispatch = useDispatch();
 	const userId = useSelector(state => state.session.user.id);
+	let contentPreview = note.content.slice(0, 50);
+	if (note.content.length > 50) {
+		contentPreview += "..."
+	}
 
 	return (
 		<div className="note-item" onClick={e => {
 			history.push("/notes");
 			return dispatch(showNote({ userId, noteId: note.id }));
 		}}>
-			<div>{note.title || "Untitled"}</div>
-			<div>{note.content}</div>
-			<div>{new Date(note.updatedAt).toTimeString()}</div>
+			<div className="note-title">{note.title || "Untitled"}</div>
+			<div className="note-content-preview">{contentPreview}</div>
+			<div className="note-timestamp">{new Date(note.updatedAt).toTimeString()}</div>
 		</div>
 	)
 }
